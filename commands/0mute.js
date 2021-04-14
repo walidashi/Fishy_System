8199ebf2d7ba7bbf2c82010e37099d30f25ffe84
walidashi <ashiwalid@gmail.com>
2021-04-14 22:28:29 +0200

    New

---
+++ commands/0mute.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require("discord.js");
+const Discord = require("discord.js")
+const sendError = require("../util/error");
+const prefix = process.env.PREFIX
+
+module.exports = {
+  info: {
+    name: "mute",
+    description: "To mute the mentioned user from text",
+    usage: "[user]",
+    aliases: ["shutup", "mute"],
+  },
+
+  run: async function (client, message, args) {
+    const role = message.guild.roles.cache.find(role => role.name === 'Muted');
+    const member = message.mentions.members.first();
+  
+    if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply(":no_entry_sign: **Error:** You don't have the **Mute Members** permission!");
+    if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
+  
+    member.roles.add(role); 
+    return message.channel.send('**Muted Successfully!**')
+}
+}
